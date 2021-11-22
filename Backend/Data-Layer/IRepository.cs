6569dff1cb102a75303188fc96bc4c20467503a9
Colin Mudie <csmudie1@gmail.com>
2021-11-22 11:36:11 -0500

    added Database Test w/ test Seed

--- Backend/Data-Layer/IRepository.cs
+++ Backend/Data-Layer/IRepository.cs
@@ -1,7 +1,14 @@
+using System.Collections.Generic;
+using Models;
+
 namespace Data_Layer
 {
     public interface IRepository
     {
-        
+        /// <summary>
+        /// This method will search the database and return a list of all users stored.
+        /// </summary>
+        /// <returns>Returns a list of Users.</returns>
+        List<User> GetAllUsers();
     }
 }
---
+++ Backend/Tests/RepositoryTest.cs
@@ -0,0 +1,141 @@
+using System;
+using System.Collections.Generic;
+using Data_Layer;
+using Microsoft.EntityFrameworkCore;
+using Models;
+using Xunit;
+
+namespace Tests
+{
+    public class RepositoryTest
+    {
+        private readonly DbContextOptions<SSDBContext> _options;
+
+        public RepositoryTest()
+        {
+            _options = new DbContextOptionsBuilder<SSDBContext>()
+                                .UseSqlite("Filename = Test.db").Options;
+            Seed();
+        }
+
+        [Fact]
+        public void GetAllUsersShouldReturnAllUsers()
+        {
+            using (var context = new SSDBContext(_options))
+            {
+                //Arrange
+                IRepository repo = new Repository(context);
+
+                //Act
+                var test = repo.GetAllUsers();
+
+                //Assert
+                Assert.NotNull(test);
+                Assert.Equal(2, test.Count);
+                Assert.Equal("Admin1", test[0].Username);
+                    //test User also contains Review
+                Assert.Equal("Admin's 1st Review", test[0].Review[0].Text);
+                    //test User also contains PreviousSearch
+                Assert.Equal("Shrek", test[0].PreviousSearch[0].Search);
+                    //test User also contains Recommendation
+                Assert.Equal("Action", test[0].Recommendation[0].Genre);
+                    //test User also contains FavoriteList
+                Assert.Equal("testImdbID1", test[0].FavoriteList[0].ImdbId);
+            }
+        }
+
+
+        // Seed Test Database
+        private void Seed()
+        {
+            using (var context = new SSDBContext(_options))
+            {
+                context.Database.EnsureDeleted();
+                context.Database.EnsureCreated();
+
+                context.Users.AddRange
+                (
+                    new User
+                    {
+                        UserId = 1,
+                        Email = "user1@admin.com",
+                        Password = "admin123",
+                        Username = "Admin1",
+                        Admin = true,
+                        Review = new List<Review>
+                        {
+                            new Review
+                            {
+                                ReviewId = 1,
+                                UserId = 1,
+                                Text = "Admin's 1st Review",
+                                Rating = 5
+                            },
+                            new Review
+                            {
+                                ReviewId = 2,
+                                UserId = 1,
+                                Text = "Admin's 2nd Review",
+                                Rating = 10
+                            }
+                        },
+                        PreviousSearch = new List<PreviousSearch>
+                        {
+                            new PreviousSearch
+                            {
+                                PreviousSearchId = 1,
+                                UserId = 1,
+                                Search = "Shrek"
+                            },
+                            new PreviousSearch
+                            {
+                                PreviousSearchId = 2,
+                                UserId = 1,
+                                Search = "Shrek 2"
+                            },
+                        },
+                        Recommendation = new List<Recommendation>
+                        {
+                            new Recommendation
+                            {
+                                RecommendationId = 1,
+                                UserId = 1,
+                                Genre = "Action"
+                            },
+                            new Recommendation
+                            {
+                                RecommendationId = 2,
+                                UserId = 1,
+                                Genre = "Horror"
+                            },
+                        },
+                        FavoriteList = new List<FavoriteList>
+                        {
+                            new FavoriteList
+                            {
+                                FavoriteListId = 1,
+                                UserId = 1,
+                                ImdbId = "testImdbID1"
+                            },
+                            new FavoriteList
+                            {
+                                FavoriteListId = 2,
+                                UserId = 1,
+                                ImdbId = "testImdbID2"
+                            },
+                        }
+                    },
+                    new User
+                    {
+                        UserId = 2,
+                        Email = "user2@test.com",
+                        Password = "user123",
+                        Username = "User2",
+                        Admin = false
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
