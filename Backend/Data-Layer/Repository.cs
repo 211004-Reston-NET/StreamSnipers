6704f98a253ece326c663d079c3cc7902919859c
Colin Mudie <csmudie1@gmail.com>
2021-11-22 12:08:14 -0500

    added 2 GetFavoriteList one by UserId other by FavoriteListId

--- Backend/Data-Layer/Repository.cs
+++ Backend/Data-Layer/Repository.cs
@@ -14,6 +14,14 @@ namespace Data_Layer
             _context = p_context;
         }
 
+        public List<FavoriteList> GetAllFavoriteListByUserId(int p_userId)
+        {
+            return _context.FavoriteLists
+                                .Where(favorite => favorite.UserId == p_userId)
+                                .AsNoTracking()
+                                .ToList();
+        }
+
         public List<User> GetAllUsers()
         {
             return _context.Users
@@ -24,14 +32,19 @@ namespace Data_Layer
                             .ToList();
         }
 
-        public User GetUserById(int p_id)
+        public FavoriteList GetFavoriteListById(int p_FavoriteListId)
+        {
+            return _context.FavoriteLists.FirstOrDefault(favorite => favorite.FavoriteListId == p_FavoriteListId);
+        }
+
+        public User GetUserById(int p_userId)
         {
             return _context.Users
                             .Include("FavoriteList")
                             .Include("PreviousSearch")
                             .Include("Recommendation")
                             .Include("Review")
-                            .FirstOrDefault(user => user.UserId == p_id);
+                            .FirstOrDefault(user => user.UserId == p_userId);
         }
     }
 }
