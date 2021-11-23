e5376c7b93f1ce2314623e2d34b1651f6c56d1af
Chase Henry <chasemh@email.sc.edu>
2021-11-23 14:17:13 -0500

    Added components
    created components for all tables and a home page.

--- FrontendAngular/src/app/app.module.ts
+++ FrontendAngular/src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Home/home/home.component';
+import { FavoriteComponent } from './Favorite/favorite/favorite.component';
+import { LoginComponent } from './Login/login/login.component';
+import { ReviewComponent } from './review/review/review.component';
+import { SearchComponent } from './search/search/search.component';
+import { RecommendComponent } from './Recommend/recommend/recommend.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FavoriteComponent,
+    LoginComponent,
+    ReviewComponent,
+    SearchComponent,
+    RecommendComponent
   ],
   imports: [
     BrowserModule
