968c1c43bc63300f56f6a3645b680f7d22569d45
Chase Henry <chasemh@email.sc.edu>
2021-11-23 16:14:02 -0500

    home page html work

--- FrontendAngular/src/app/app.module.ts
+++ FrontendAngular/src/app/app.module.ts
@@ -5,8 +5,8 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './Home/home/home.component';
 import { FavoriteComponent } from './Favorite/favorite/favorite.component';
 import { LoginComponent } from './Login/login/login.component';
-import { ReviewComponent } from './review/review/review.component';
-import { SearchComponent } from './search/search/search.component';
+import { ReviewComponent } from './Review/review/review.component';
+import { SearchComponent } from './Search/search/search.component';
 import { RecommendComponent } from './Recommend/recommend/recommend.component';
 
 @NgModule({
