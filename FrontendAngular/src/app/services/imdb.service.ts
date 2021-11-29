8658d00106dbd08c10b09f23a0b3546f0f0c59b6
Colin Mudie <csmudie1@gmail.com>
2021-11-29 12:32:44 -0500

    added streamAPI service

--- FrontendAngular/src/app/services/imdb.service.ts
+++ FrontendAngular/src/app/services/imdb.service.ts
@@ -9,6 +9,7 @@ export class ImdbService {
   constructor(private http:HttpClient) { }
   private endpoint:string = 'https://imdb-api.com/api/search/k_shxfmft9/';
 
+  // search parameter: should be the string from the search bar form at the top of the page.
   imdbMovieSearch(search:string)
   {
     return this.http.get(this.endpoint+search)
