62420631d1e44b25844d7e51f539ff6b70f62b53
Colin Mudie <csmudie1@gmail.com>
2021-11-29 14:52:46 -0500

    Revert "Delete Backend directory"
    This reverts commit 60c31c6eea4574ca9f5afd354b90e6c9873d59a8.

---
+++ Backend/WebAPI/Controllers/ReviewController.cs
@@ -0,0 +1,58 @@
+﻿using Data_Layer;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReviewController : ControllerBase
+    {
+        private readonly IRepository _repo;
+
+        public ReviewController(IRepository p_repo)
+        {
+            _repo = p_repo;
+        }
+
+        // GET api/<ReviewController>/5
+        [HttpGet("{id}")]
+        public IActionResult GetReviewById(int id)
+        {
+            return Ok(_repo.GetReviewById(id));
+        }
+
+        // GET api/<ReviewController>/User/5
+        [HttpGet("User/{id}")]
+        public IActionResult GetReviewByUserId(int id)
+        {
+            return Ok(_repo.GetReviewByUserId(id));
+        }
+
+        // POST api/<ReviewController>
+        [HttpPost("Add")]
+        public IActionResult CreateReview([FromBody] Review value)
+        {
+            return Created("api/Review/Add", _repo.AddReview(value));
+        }
+
+        //// PUT api/<ReviewController>/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
+
+        // DELETE api/<ReviewController>/5
+        [HttpDelete("{id}")]
+        public IActionResult DeleteReviewById(int id)
+        {
+            return Ok(_repo.DeleteReviewById(id));
+        }
+    }
+}
