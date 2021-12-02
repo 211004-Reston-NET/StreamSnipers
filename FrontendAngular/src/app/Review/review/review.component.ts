e7827b4765cb7cfbe76a92470783e7d36d3c144d
Colin Mudie <csmudie1@gmail.com>
2021-12-02 15:10:03 -0500

    fixed userId to change to who is logged in.

--- FrontendAngular/src/app/Review/review/review.component.ts
+++ FrontendAngular/src/app/Review/review/review.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '@auth0/auth0-angular';
 import { Review } from 'src/app/models/review';
 import { WebAPIService } from 'src/app/services/web-api.service';
 
@@ -9,23 +10,31 @@ import { WebAPIService } from 'src/app/services/web-api.service';
   styleUrls: ['./review.component.css']
 })
 export class ReviewComponent implements OnInit {
-  constructor(private webAPI:WebAPIService) { }
+  constructor(private webAPI: WebAPIService, public auth0: AuthService) { }
   currentRate = 0;
-
+  private userId:number = 0;
   reviewGroup:FormGroup = new FormGroup({
     text: new FormControl("", Validators.required)
   });
 
   ngOnInit(): void {
+    // This is how we find the userId of who is logged in.
+    this.auth0.user$.subscribe((user) => {
+      if (user) {
+        this.webAPI.getUserIdByEmail(user.email).subscribe((userId) => {
+          this.userId = userId;
+        });
+      }
+    });
   }
 
   createReview(revGroup:FormGroup)
   { 
     if (revGroup.valid) 
     {
+      
       let review:Review = {
-        //setting userId to 1 just to test
-        UserId: 1,
+        UserId: this.userId,
         Text: revGroup.get("text")?.value,
         Rating: this.currentRate
       }
