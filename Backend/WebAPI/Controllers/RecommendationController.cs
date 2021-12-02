0458e1396692524caf25d7ae5f0600e59241a3b7
Chase Henry <chasemh@email.sc.edu>
2021-12-02 16:31:16 -0500

    logging in recommendation controller.

--- Backend/WebAPI/Controllers/RecommendationController.cs
+++ Backend/WebAPI/Controllers/RecommendationController.cs
@@ -5,6 +5,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Serilog;
+using Serilog.Formatting.Json;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -24,21 +26,72 @@ namespace WebAPI.Controllers
         [HttpGet("{id}")]
         public IActionResult GetRecommendationById(int id)
         {
-            return Ok(_repo.GetRecommendationById(id));
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .WriteTo.File(new JsonFormatter(),"Logs/GetRecommendationByIdlog.json")
+                .CreateLogger();
+            try
+            {    
+                Log.Information("");
+                return Ok(_repo.GetRecommendationById(id));
+            }
+            catch
+            {
+                Log.Information("");
+                return null;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         // GET api/recommendation/user/{id}
         [HttpGet("User/{id}")]
         public IActionResult GetRecommendationByUserId(int id)
         {
-            return Ok(_repo.GetRecommendationByUserId(id));
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .WriteTo.File(new JsonFormatter(),"Logs/GetRecommendationByUserIdlog.json")
+                .CreateLogger();
+            try
+            {
+                Log.Information("Get Recommendation By User id");
+                return Ok(_repo.GetRecommendationByUserId(id));
+            }
+            catch
+            {
+                Log.Information("Failed getting recommendation by user id");
+                return null;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         // POST api/recommendation/add
         [HttpPost("Add")]
         public IActionResult AddRecommendation([FromBody] Recommendation value)
         {
-            return Created("api/Recommendation/Add", _repo.AddRecommendation(value));
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .WriteTo.File(new JsonFormatter(),"Logs/AddRecommendationlog.json")
+                .CreateLogger();
+            try
+            {
+                Log.Information("Adding Recommendation");
+                return Created("api/Recommendation/Add", _repo.AddRecommendation(value));
+            }
+            catch
+            {
+                Log.Information("Failed adding recommendation");
+                return null;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         //// PUT api/<RecommendationController>/5
@@ -51,7 +104,24 @@ namespace WebAPI.Controllers
         [HttpDelete("{id}")]
         public IActionResult DeleteRecommendationById(int id)
         {
-            return Ok(_repo.DeleteRecommendationById(id));
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .WriteTo.File(new JsonFormatter(),"Logs/DeleteRecommendationByIdlog.json")
+                .CreateLogger();
+            try
+            {
+                Log.Information("Deleting Recommendation by id");
+                return Ok(_repo.DeleteRecommendationById(id));
+            }
+            catch
+            {
+                Log.Information("Failed Deleting Recommendation by id");
+                return null;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
 
@@ -59,8 +129,25 @@ namespace WebAPI.Controllers
         [HttpPut("update-recommendation-by-id/{id}")]
         public IActionResult UpdateRecommendationById(int id, [FromBody] Recommendation rec)
         {
-            var updateRecommendation = _repo.UpdateRecommendationById(id, rec);
-            return Ok(updateRecommendation);
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .WriteTo.File(new JsonFormatter(),"Logs/UpdateRecommendationByIdlog.json")
+                .CreateLogger();
+            try
+            {
+                Log.Information("Updating recommendation by id");
+                var updateRecommendation = _repo.UpdateRecommendationById(id, rec);
+                return Ok(updateRecommendation);
+            }
+            catch
+            {
+                Log.Information("failed updating recommendation by id");
+                return null;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
     }
 }
