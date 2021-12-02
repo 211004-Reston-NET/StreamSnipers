da1260e17ad9d8baf8aa12b54a242500bd18f61e
Chase Henry <chasemh@email.sc.edu>
2021-12-02 15:59:30 -0500

    added logging to favoritelist controller

--- Backend/WebAPI/Controllers/FavoriteListController.cs
+++ Backend/WebAPI/Controllers/FavoriteListController.cs
@@ -5,6 +5,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Serilog;
+using Serilog.Formatting.Json;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -24,28 +26,98 @@ namespace WebAPI.Controllers
         [HttpGet("All")]
         public IActionResult GetAllFavoriteList()
         {
-            return Ok(_repo.GetAllFavoriteList());
+             Log.Logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .WriteTo.File(new JsonFormatter(),"Logs/allfavoritelistlog.json")
+                .CreateLogger();
+
+            try 
+            {
+                Log.Information("Favorite List acquired");
+                return Ok(_repo.GetAllFavoriteList());
+            }
+            catch
+            {
+                Log.Information("Failed getting all favorite list");
+                return null;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+
         }
 
         // GET api/favoriteList/{id}
         [HttpGet("{id}")]
         public IActionResult GetFavoriteListById(int id)
         {
-            return Ok(_repo.GetFavoriteListById(id));
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .WriteTo.File(new JsonFormatter(),"Logs/favoritelistbyidlog.json")
+                .CreateLogger();
+            try
+            {
+                Log.Information("Favorite List by id acquired");
+                return Ok(_repo.GetFavoriteListById(id));
+            }
+            catch
+            {
+                Log.Information("Failed getting all favorite list by id");
+                return null;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         // GET api/favoriteList/user/{id}
         [HttpGet("User/{id}")]
         public IActionResult GetFavoriteListByUserId(int id)
         {
-            return Ok(_repo.GetFavoriteListByUserId(id));
+             Log.Logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .WriteTo.File(new JsonFormatter(),"Logs/favoritelistbyuseridlog.json")
+                .CreateLogger();
+            try
+            {
+                Log.Information("Favorite List by user Id acquired");
+                return Ok(_repo.GetFavoriteListByUserId(id));
+            }
+            catch
+            {
+                Log.Information("Failed getting favorite list by id");
+                return null;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         // POST api/favoriteList/add
         [HttpPost("Add")]
         public IActionResult AddFavoriteList([FromBody] FavoriteList value)
         {
-            return Created("api/FavoriteList/Add", _repo.AddFavoriteList(value));
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .WriteTo.File(new JsonFormatter(),"Logs/addfavoritelistlog.json")
+                .CreateLogger();
+            try
+            {
+                Log.Information("Success adding favorite list");
+                return Created("api/FavoriteList/Add", _repo.AddFavoriteList(value));
+            }
+            catch
+            {
+                Log.Information("Failed adding favorite list");
+                return null;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         //// PUT api/favoriteList/{id}
@@ -58,7 +130,24 @@ namespace WebAPI.Controllers
         [HttpDelete("{id}")]
         public IActionResult DeleteFavoriteListById(int id)
         {
-            return Ok(_repo.DeleteFavoriteListById(id));
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .WriteTo.File(new JsonFormatter(),"Logs/deletefavoritelistlog.json")
+                .CreateLogger();
+            try
+            {
+                Log.Information("");
+                return Ok(_repo.DeleteFavoriteListById(id));
+            }
+            catch
+            {
+                Log.Information("");
+                return null;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
 
@@ -66,8 +155,25 @@ namespace WebAPI.Controllers
         [HttpPut("update-favoritelist-by-id/{id}")]
         public IActionResult UpdateFavoriteListById(int id, [FromBody] FavoriteList fList)
         {
-            var updateFavoriteList = _repo.UpdateFavoriteListById(id, fList);
-            return Ok(updateFavoriteList);
+             Log.Logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .WriteTo.File(new JsonFormatter(),"Logs/updatefavoritelistlog.json")
+                .CreateLogger();
+            try
+            {
+                Log.Information("");
+                var updateFavoriteList = _repo.UpdateFavoriteListById(id, fList);
+                return Ok(updateFavoriteList);
+            }
+            catch
+            {
+                Log.Information("");
+                return null;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
     }
 }
