a046cfd928cde0467bb37dab74ceda3e871f60ec
Colin Mudie <csmudie1@gmail.com>
2021-12-03 19:04:03 -0500

    allowed favorite btn to change if favorited

--- FrontendAngular/src/app/Home/home/home.component.ts
+++ FrontendAngular/src/app/Home/home/home.component.ts
@@ -13,14 +13,19 @@ import { WebAPIService } from 'src/app/services/web-api.service';
 })
 export class HomeComponent implements OnInit {
   searchtext: string = '';
+  userId: number = 0;
+
+  @Output()
+  isFavorite: boolean = false;
+
   imdbId: string = '';
   movePosterSrc: string = '';
   movieTitleSrc: string = '';
   movieReleaseDateSrc: string = '';
   movieRuntimeSrc: string = '';
   movieDescriptionSrc: string = '';
-  movieAwardsSrc: string ='';
-  favorite: FavoriteList = {
+  movieAwardsSrc: string = '';
+  newFavorite: FavoriteList = {
     favoriteListId: 0,
     userId: 0,
     imdbId: '',
@@ -31,7 +36,8 @@ export class HomeComponent implements OnInit {
     hboMax: false,
     disneyPlus: false
   };
-  @Input()                    
+  favoriteList: FavoriteList[] = [];
+  @Input()
   selected: boolean = false;
   @Output() selectedChange = new EventEmitter<boolean>();
 
@@ -39,52 +45,57 @@ export class HomeComponent implements OnInit {
 
 
   constructor
-  (
-    public imdbAPI: ImdbService, 
-    public auth0:AuthService, 
-    @Inject(DOCUMENT) public document: Document, 
-    public webAPI: WebAPIService, 
-    private router: Router
-  ) 
-  {
-    this.favorite.userId = this.webAPI.getId();
+    (
+      public imdbAPI: ImdbService,
+      public auth0: AuthService,
+      @Inject(DOCUMENT) public document: Document,
+      public webAPI: WebAPIService,
+      private router: Router
+    ) {
+    this.newFavorite.userId = this.webAPI.getId();
     this.searchtext = this.imdbAPI.movieTitle;
-  }
-
-  ngOnInit(): void {
     this.auth0.user$.subscribe(
-      (profile) => 
-      {
-        if(profile)
-        {
-        // check if user is in db already
+      (profile) => {
+        if (profile) {
+          // check if user is in db already
           this.webAPI.loginUser(profile.email).subscribe(
-            (response) => 
-            {
-              if (!response)
-              {
+            (response) => {
+              if (!response) {
                 // post new user here using profile.email 
               }
-              else 
-              {
-                this.favorite.userId = response.userId;
+              else {
+                this.newFavorite.userId = response.userId;
+                this.userId = response.userId;
               }
             }
           )
         }
       }
-      );
+    );
+  }
+
+  ngOnInit(): void {
+    // check db to see if movie is favorited already
+    this.webAPI.getFavoriteListByUserId().then((response) => {
+      this.favoriteList = this.favoriteList.concat(response);
+    });
     // runs the movie search if approaching from clicked favorite
-    if (this.searchtext)
-    {
+    if (this.searchtext) {
       this.imdbSearch(this.searchtext);
     }
+
+
   }
 
   imdbSearch(search: string) {
     this.imdbAPI.imdbIdSearch(search).subscribe(
       (response) => {
         this.imdbId = response.results[0].id;
+        this.favoriteList.forEach(element => {
+          if (element.imdbId == response.results[0].id) {
+            this.isFavorite = true;
+          }
+        });
         if (this.imdbId) {
           this.imdbAPI.imdbMovieSearch(this.imdbId).subscribe(
             (res) => {
@@ -96,27 +107,38 @@ export class HomeComponent implements OnInit {
               this.movieDescriptionSrc = res.plot;
               this.movieAwardsSrc = res.awards;
               this.imdbAPI.movieTitle = res.title;
-                // changing favorite in case they click add to favorite
-              this.favorite.name = this.movieTitleSrc;
-              this.favorite.imdbId = this.imdbId;
+              // changing favorite in case they click add to favorite
+              this.newFavorite.name = this.movieTitleSrc;
+              this.newFavorite.imdbId = this.imdbId;
             }
           )
         }
       })
   }
 
-  addToFavorites()
-  {
-    console.log(this.favorite);
-    this.webAPI.createFavorite(this.favorite).subscribe(
+  addToFavorites() {
+    this.webAPI.createFavorite(this.newFavorite).subscribe(
       (response) => {
-        console.log(response);
+        this.isFavorite = true;
       });
   }
 
-  goToReview() 
-  {
+  goToReview() {
     this.router.navigateByUrl("/review");
+  }
+
+  deleteFavorite() {
+    let id = 0;
+    this.favoriteList.forEach(element => {
+      if (element.imdbId == this.imdbId) {
+        id = element.favoriteListId;
+      }
+    });
+    this.webAPI.deleteFavorite(id).subscribe(
+      (response) => {
+        this.isFavorite = false;
+      }
+    )
   }
 
   public toggleSelected() {
